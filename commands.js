aa13b7e6f8085f8addda2b4aebca412a1cac8838
Griffith <jangrabara@gmail.com>
2018-05-09 21:41:51 +0200

    command.js zipstatic

--- commands.js
+++ commands.js
@@ -5,7 +5,7 @@ const zipdir = require('zip-dir')
 const request = require('request')
 const fs = require('fs')
 const path = require('path')
-const { dirAdress } = require('./staticConfig')
+const { dirAdress, staticAdress } = require('./staticConfig')
 
 const filertNode_modules = (path, stat) =>
   !path.endsWith('node_modules')
@@ -21,12 +21,14 @@ function getApiKey(configPath) {
 }
 
 program
-  .command('zip <dir>')
-  .action(async (dir, cmd) => {
+  .command('zipapp [otherDirs...]')
+  .action((otherDirs) => {
+
+    const dir = otherDirs[0] ? otherDirs[0].toString() : ''
 
     const filePath = path.join(process.cwd(), dir)
     const configPath = path.join(filePath, "staticConfig.json")
-    
+
     if (fs.existsSync(filePath)) {
 
       const apiKey = getApiKey(configPath)
@@ -36,13 +38,56 @@ program
         process.exit()
       }
 
-      zipdir(filePath, { filter: filertNode_modules }, function (err, buffer) {
+      zipdir(filePath, { filePath: filertNode_modules }, function (err, buffer) {
         if (err) {
           console.error(err)
           process.exit()
         }
 
         const r = request.post(dirAdress, function (err, httpResponse, body) {
+          if (err) {
+            return console.error('upload failed:', err);
+          }
+          console.log('Upload successful!  Server responded with:', body);
+          process.exit()
+        });
+        const form = r.form()
+        form.append('file', buffer, { filename: 'zip.zip' })
+        form.append('apiKey', apiKey)
+
+      })
+
+    } else {
+      console.error('No such dir')
+      process.exit()
+    }
+  })
+
+program
+  .command('zipstatic [otherDirs...]')
+  .action((otherDirs) => {
+
+    const dir = otherDirs[0] ? otherDirs[0].toString() : ''
+
+    const filePath = path.join(process.cwd(), dir)
+    const configPath = path.join(filePath, "staticConfig.json")
+
+    if (fs.existsSync(filePath)) {
+
+      const apiKey = getApiKey(configPath)
+
+      if (!apiKey) {
+        console.error("No api-key provided")
+        process.exit()
+      }
+
+      zipdir(filePath, { filePath: filertNode_modules }, function (err, buffer) {
+        if (err) {
+          console.error(err)
+          process.exit()
+        }
+
+        const r = request.post(staticAdress, function (err, httpResponse, body) {
           if (err) {
             return console.error('upload failed:', err);
           }
