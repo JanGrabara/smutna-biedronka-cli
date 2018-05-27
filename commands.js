c1c37096355d2d21e7ff2e9837013240de470eb7
Griffith <jangrabara@gmail.com>
2018-05-27 20:55:47 +0200

    command.js createconfig, config route

--- commands.js
+++ commands.js
@@ -3,16 +3,41 @@ const program = require('commander');
 const { prompt } = require('inquirer');
 const request = require('request')
 const { getBufferAndApiKey } = require('./zip')
+const fs = require('fs')
 
+const questions = [
+  {
+    type: 'input',
+    name: 'apiKey',
+    message: 'apiKey'
+  },
+  {
+    type: 'input',
+    name: 'uploadUrl',
+    message: 'uploadUrl'
+  }
+];
 
 program
-  .command('zipapp [otherDirs...]')
-  .action(async (otherDirs) => {
+  .command('createconfig')
+  .alias('cc')
+  .description('Create biedronka.config.json')
+  .action(() => {
+    prompt(questions).then(answers => {
+      fs.writeFileSync('biedronka.config.json', JSON.stringify(answers), 'utf8')
+      process.exit()
+    });
+  });
 
+program
+  .command('push:app [otherDirs...]')
+  .description('Arg: path to folder to zip relative to cli path, not required default get cli path')
+  .option('-c, --config', 'biedronka.config.json in cli folder (default get config from zip folder)')
+  .action(async (otherDirs, cmd) => {
     let buffer, apiKey, uploadUrl
 
     try {
-      const { bufferr, config } = await getBufferAndApiKey(otherDirs)
+      const { bufferr, config } = await getBufferAndApiKey(otherDirs, cmd.config)
       buffer = bufferr
       apiKey = config.apiKey
       uploadUrl = config.uploadUrl + "/api/Apps/AddAppFiles"
@@ -35,13 +60,15 @@ program
   })
 
 program
-  .command('zipstatic [otherDirs...]')
-  .action(async otherDirs => {
+  .command('push:static [otherDirs...]')
+  .description('Arg: path to folder to zip relative to cli path, not required default get cli path')
+  .option('-c, --config', 'biedronka.config.json in cli folder (default get config from zip folder)')
+  .action(async (otherDirs, cmd) => {
 
     let buffer, apiKey, uploadUrl
 
     try {
-      const { bufferr, config } = await getBufferAndApiKey(otherDirs)
+      const { bufferr, config } = await getBufferAndApiKey(otherDirs, cmd.config)
       buffer = bufferr
       apiKey = config.apiKey
       uploadUrl = config.uploadUrl + "/api/StaticContent/AddFiles"
